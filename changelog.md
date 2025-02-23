9583d3115aa4d94e8f24d831323100e11a9fc0f0
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2025-02-23 16:14:41 -0500

    index compliance

--- changelog.md
+++ changelog.md
@@ -1,2 +1,4 @@
+# 1.0.1
+- Make the mod more index-compliant.
 # 1.0.0
 - Initial release
--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,9 @@
-#include <Geode/Geode.hpp>
 #include <Geode/modify/LevelSearchLayer.hpp>
-#include <Geode/utils/cocos.hpp>
-
 using namespace geode::prelude;
-
 class $modify(LevelSearchLayer) {
     bool init(int searchType) {  
-        if (!LevelSearchLayer::init(searchType)) {
-            return false;
-        }
-
-        log::info("LevelSearchLayer initialized!");
-
-        // Find the quick search menu
-        auto menu = this->getChildByID("quick-search-menu");
-        if (menu) {
-            log::info("Found quick-search-menu!");
-
-            // Try to find the "Recent" button using its ID
-            auto recentButton = menu->getChildByID("recent-button");
-            if (recentButton) {
-                log::info("Found recent-button! Removing it...");
-                recentButton->removeFromParent();
-            }
-            else {
-                log::info("recent-button not found inside quick-search-menu.");
-            }
-        }
-        else {
-            log::info("quick-search-menu not found!");
-        }
-
+        if (!LevelSearchLayer::init(searchType)) return false;
+        if (auto recentButton = this->querySelector("quick-search-menu > recent-button")) recentButton->setInvisible(false);
         return true;
     }
 };
